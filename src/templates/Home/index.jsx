9b4f160912c862ce9f0c85a2cce2b24fbdff0152
André Pedro <sincdor@gmail.com>
2022-05-03 17:45:51 +0100

    Empty commit

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -14,6 +14,8 @@ export function Home() {
   const [postsPerPage, setPostsPerPage] = useState(10);
   const [searchValue, setSearchValue] = useState('');
 
+  //Comment
+
   const handleLoadPosts = useCallback(async (page, postsPerPage) => {
     const postsAndPhotos = await loadPosts();
     setPosts(postsAndPhotos.slice(page, postsPerPage));
