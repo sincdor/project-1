fd1b052820fcc980cfe9573807d6159a91f090f6
André Pedro <sincdor@gmail.com>
2022-05-03 16:36:56 +0100

    Class component to function

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,84 +1,71 @@
-import { Component } from 'react';
-
 import './styles.css';
+
+import { useEffect, useState, useCallback } from 'react';
 
 import { Posts } from '../../components/Posts';
 import { loadPosts } from '../../utils/load-posts';
 import Button from '../../components/Button';
 import { TextInput } from '../../components/TextInput';
 
-export class Home extends Component {
-  state = {
-    posts: [],
-    allPosts: [],
-    page: 0,
-    postsPerPage: 10,
-    searchValue: '',
-  };
+export function Home() {
+  const [posts, setPosts] = useState([]);
+  const [allPosts, setAllPosts] = useState([]);
+  const [page, setPage] = useState(0);
+  const [postsPerPage, setPostsPerPage] = useState(10);
+  const [searchValue, setSearchValue] = useState('');
 
-  async componentDidMount() {
-    await this.loadPosts();
-  }
-
-  loadPosts = async () => {
-    const { page, postsPerPage } = this.state;
+  const handleLoadPosts = useCallback(async (page, postsPerPage) => {
     const postsAndPhotos = await loadPosts();
-    this.setState({
-      posts: postsAndPhotos.slice(page, postsPerPage),
-      allPosts: postsAndPhotos,
-    });
-  };
+    setPosts(postsAndPhotos.slice(page, postsPerPage));
+    setAllPosts(postsAndPhotos);
+  }, []);
 
-  loadMorePosts = () => {
-    const { page, postsPerPage, allPosts, posts } = this.state;
+  useEffect(() => {
+    handleLoadPosts(0, postsPerPage);
+  }, [handleLoadPosts, postsPerPage]);
+
+  const loadMorePosts = () => {
     const nextPage = page + postsPerPage;
 
     const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
     posts.push(...nextPosts);
 
-    this.setState({
-      posts,
-      page: nextPage,
-    });
+    setPosts(posts);
+    setPage(nextPage);
   };
 
-  handleChange = (e) => {
+  const handleChange = (e) => {
     const { value } = e.target;
-    this.setState({ searchValue: value });
+    setSearchValue(value);
   };
 
-  render() {
-    const { posts, page, postsPerPage, allPosts, searchValue } = this.state;
-    const noMorePosts = page + postsPerPage >= allPosts.length;
+  const noMorePosts = page + postsPerPage >= allPosts.length;
+  const filteredPosts = filterPosts(searchValue, allPosts, posts);
 
-    const filteredPosts = this.filterPosts(searchValue, allPosts, posts);
+  return (
+    <section className='container'>
+      <div className='search-container'>
+        {searchValue && <h1>Search value: {searchValue}</h1>}
+        {<TextInput onChange={handleChange} value={searchValue} />}
+      </div>
 
-    return (
-      <section className='container'>
-        <div className='search-container'>
-          {searchValue && <h1>Search value: {searchValue}</h1>}
-          {<TextInput onChange={this.handleChange} value={searchValue} />}
-        </div>
+      {filteredPosts.length > 0 && <Posts posts={filteredPosts} />}
+      {filteredPosts.length === 0 && <p>Não existem posts</p>}
+      <div className='button-container'>
+        <Button
+          text='Load more posts'
+          onClick={loadMorePosts}
+          disabled={noMorePosts}
+        />
+      </div>
+    </section>
+  );
+}
 
-        {filteredPosts.length > 0 && <Posts posts={filteredPosts} />}
-        {filteredPosts.length === 0 && <p>Não existem posts</p>}
-        <div className='button-container'>
-          <Button
-            text='Load more posts'
-            onClick={this.loadMorePosts}
-            disabled={noMorePosts}
-          />
-        </div>
-      </section>
-    );
-  }
-
-  filterPosts(searchValue, allPosts, defaultPosts) {
-    console.log(defaultPosts.length);
-    return searchValue
-      ? allPosts.filter((post) => {
-          return post.title.toLowerCase().includes(searchValue.toLowerCase());
-        })
-      : defaultPosts;
-  }
+function filterPosts(searchValue, allPosts, defaultPosts) {
+  return searchValue
+    ? allPosts.filter((post) =>
+        post.title.toLowerCase().includes(searchValue.toLowerCase())
+      )
+    : defaultPosts;
 }
